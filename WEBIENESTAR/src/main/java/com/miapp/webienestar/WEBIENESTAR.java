3990528e1908fc0c8cd22be4bbfafbaffe823ed2
Denise Agostina Geraldine Diaz <dgeraldineunviersidad@gmail.com>
2024-09-14 19:23:15 -0300

    Modificaciones

---
+++ WEBIENESTAR/src/main/java/com/miapp/webienestar/WEBIENESTAR.java
@@ -0,0 +1,11 @@
+package com.miapp.webienestar;
+
+public class WEBIENESTAR {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    //Conexion conn = Conexion(); Chequear como importar 
+
+}
