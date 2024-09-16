c95545e99f111ed7833a78956fa71edc0f789b42
Nicolas Juan Ignacio Torres <nicoignacio123@gmail.com>
2024-09-16 02:39:52 -0300

    FEAT: organizando la estructura

---
+++ Backend/webienestarAPI/src/main/java/IS/webienestar/WebienestarApplication.java
@@ -0,0 +1,13 @@
+package IS.webienestar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebienestarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebienestarApplication.class, args);
+	}
+
+}
---
+++ Backend/webienestarAPI/src/test/java/IS/webienestar/WebienestarApplicationTests.java
@@ -0,0 +1,13 @@
+package IS.webienestar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebienestarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
