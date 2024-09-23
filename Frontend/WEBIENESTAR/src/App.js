7d39b2b840ebfffe550c74c40e29a920273f1de8
lucianafigue <mlfluli@gmail.com>
2024-09-23 10:11:13 -0300

    Vistas de Beca Comedor, Reserva Vianda, Retroalimentacion Vianda y Registro

--- Frontend/WEBIENESTAR/src/App.js
+++ Frontend/WEBIENESTAR/src/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login'; // Vista de inicio de sesión
 import Register from './components/Register-Form'; // Vista de registro
+import BecaComedor from './components/BecaComedor-Form';
+import ReservarVianda from './components/ReservarVianda';
+import RetroVianda from './components/RetroVianda';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/BecaComedor" element={<BecaComedor />} />
+        <Route path="/ReservarVianda" element={<ReservarVianda />} />
+        <Route path="/RetroVianda" element={<RetroVianda />} />
       </Routes>
     </Router>
   );
